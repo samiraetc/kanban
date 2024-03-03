f5eb2f8237ec7a19bf692d427fe7e02367a6cfdd
Samira Costa <samiramanuelac@gmail.com>
2024-03-03 18:07:08 +0000

    feat: removed static infos and implemented requests

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Column, { ColumnTypes } from "../Column/Column";
 import Card, { CardTypes } from "../Card/Card";
 import Modal from "../Modal/Modal";
+import useFetch from "@/hook/useFetch";
+import { error } from "console";
 
 const columns = [
     {
@@ -18,67 +20,89 @@ const columns = [
     },
 ];
 
-const cards = [
-    {
-        id: "1",
-        titulo: "Realizar teste",
-        conteudo: "Conteudo",
-        lista: "to_do",
-    },
-    {
-        id: "2",
-        titulo: "Gravar video",
-        conteudo: "Conteudo",
-        lista: "to_do",
-    },
-
-    {
-        id: "3",
-        titulo: "Fazer almoço",
-        conteudo: "Conteudo",
-        lista: "doing",
-    },
-
-    {
-        id: "4",
-        titulo: "Academia",
-        conteudo: "Conteudo",
-        lista: "done",
-    },
-];
-
 const Board = () => {
-    const [tasks, setTasks] = useState(cards);
-    const [open, setOpen] = useState<boolean>(false)
+    const { getCards, updateCard, removeCard, addCard } = useFetch();
+    const [tasks, setTasks] = useState<CardTypes[]>();
+    const [open, setOpen] = useState<boolean>(false);
+
+
+
+    useEffect(() => {
+        (async () => {
+            setTasks(await getCards());
+        })();
+    }, []);
+
+
 
     const handleUpdateTask = (item: CardTypes) => {
-        const updatedCard = tasks.map((card) => {
-            return card.id === item.id ? item : card;
-        });
-
-        setTasks(updatedCard);
+        setTasks((prevTasks) =>
+            prevTasks?.map((card) => (card.id === item.id ? item : card))
+        );
     };
 
-    const handleDrop = (e: React.DragEvent<HTMLDivElement>, key: string) => {
+    const handleDrop = async (
+        e: React.DragEvent<HTMLDivElement>,
+        key: string
+    ) => {
         e.preventDefault();
         const cardId = e.dataTransfer.getData("id");
-        const cardFiltered = tasks.find((card) => card.id === cardId);
-        cardFiltered && handleUpdateTask({ ...cardFiltered, lista: key });
+        const cardFiltered = tasks?.find((card) => card.id === cardId);
+
+        if (cardFiltered) {
+
+            handleUpdateTask({ ...cardFiltered, lista: key });
+            await updateCard({ ...cardFiltered, lista: key });
+
+        }
     };
 
-    const handleDeleteCard = (id: string) => {
-        console.log(id)
-    }
+    const handleDeleteCard = async (id: string) => {
+        const remove = tasks?.find((task) => task.id === id);
 
-    const handleEditCard = (item: { id: string, title: string, description: string }) => {
-        const cardFiltered = tasks.find((card) => card.id === item.id);
-        cardFiltered && handleUpdateTask({ ...cardFiltered, titulo: item.title, conteudo: item.description });
-    }
+
+
+        remove && setTasks(await removeCard(id));
+
+    };
+
+    const handleEditCard = async (item: {
+        id: string;
+        title: string;
+        description: string;
+    }) => {
+        const cardFiltered = tasks?.find((card) => card.id === item.id);
+
+
+        if (cardFiltered) {
+            await updateCard({
+                ...cardFiltered,
+                titulo: item.title,
+                conteudo: item.description,
+            });
+
+            setTasks(
+                tasks?.map((task) =>
+                    task.id === item.id
+                        ? { ...task, titulo: item.title, conteudo: item.description }
+                        : task
+                )
+            );
+        }
+
+    };
 
     const handleCloseModal = () => {
-        setOpen(false)
-    }
+        setOpen(false);
+    };
 
+    const handleAddNewCard = async (
+        title: string,
+        description: string,
+        list: string
+    ) => {
+        await addCard({ titulo: title, conteudo: description, lista: list });
+    };
 
     return (
         <div className="bg-primary-gray p-4 m-6 rounded-lg flex justify-between">
@@ -91,21 +115,27 @@ const Board = () => {
                         }}
                         onDragOver={(e) => e.preventDefault()}
                     >
-
                         <Column column={column} key={columnIndex}>
                             {column.key === "to_do" && (
                                 <div
                                     className="text-center bg-gray-200 p-2 text-gray-500 rounded-lg hover:bg-gray-300 "
                                     onClick={() => setOpen(true)}
-
+                                    key={columnIndex}
                                 >
-                                    <p className=" text-sm font-semibold ">+ Adicionar um cartão</p>
+                                    <p className=" text-sm font-semibold ">
+                                        + Adicionar um cartão
+                                    </p>
                                 </div>
                             )}
-                            {tasks.map((card: CardTypes, cardIndex: number) => {
+                            {tasks?.map((card: CardTypes, cardIndex: number) => {
                                 return (
                                     card.lista === column.key && (
-                                        <Card card={card} key={cardIndex} handleDeleteCard={handleDeleteCard} handleEditCard={handleEditCard} />
+                                        <Card
+                                            card={card}
+                                            key={cardIndex}
+                                            handleDeleteCard={handleDeleteCard}
+                                            handleEditCard={handleEditCard}
+                                        />
                                     )
                                 );
                             })}
@@ -114,10 +144,8 @@ const Board = () => {
                 );
             })}
 
-            <Modal open={open} close={handleCloseModal} />
+            <Modal open={open} close={handleCloseModal} addNew={handleAddNewCard} />
         </div>
-
-
     );
 };
 
